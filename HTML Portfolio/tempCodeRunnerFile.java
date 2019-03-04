e70a13342475fe9d168df38c768cf926c4a93bbf
Juan-Diego <juandiego085@gmail.com>
2019-03-04 09:25:03 -0500

    small changes

---
+++ HTML Portfolio/tempCodeRunnerFile.java
@@ -0,0 +1,8 @@
+public class ma{
+    public static void mian (String [] args){
+        ma m = new ma (); 
+        String name = "Juan"; 
+        int [] sum = {1,6,63,4}; 
+        System.out.println (sum.length() + " " + name.length () );         
+    }
+}
